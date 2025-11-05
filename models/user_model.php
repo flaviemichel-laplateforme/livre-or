46479e377dffacf316968521acfab4ed74961e64
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-11-05 16:01:03 +0100

    Ajout de la fonction create_user pour gérer la création d'un utilisateur avec mot de passe haché dans le modèle utilisateur.

--- models/user_model.php
+++ models/user_model.php
@@ -1,1 +1,13 @@
-<!-- modèle utilisateur (CRUD) -->
+<?php
+
+function create_user($login, $password)
+{
+
+    $hashed_password = hash_password($password);
+    $query = "INSERT INTO utilisateurs (login,password) VALUES (? ,? ) ";
+
+    if (db_execute($query, [$login, $hashed_password])) {
+        return db_last_insert_id();
+    }
+    return false;
+}
