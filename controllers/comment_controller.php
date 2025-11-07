c09bf28db65b34337605920898dae56d13e62f2c
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-11-07 14:38:36 +0100

    Ajout de la fonction comment_create pour gérer la création de commentaires, incluant la validation et l'insertion en base de données. Mise à jour de la vue pour afficher le formulaire de commentaire.

--- controllers/comment_controller.php
+++ controllers/comment_controller.php
@@ -15,4 +15,55 @@ function comment_livre_or()
     ]);
 }
 
-// (Vous ajouterez d'autres fonctions comme comment_create() ici plus tard)
+
+
+
+/**
+ * Affiche le formulaire de création de commentaire (en GET)
+ * et traite la soumission du formulaire (en POST).
+ * URL: /comment/create
+ */
+function comment_create()
+{
+
+    require_login();
+
+
+    if (is_post()) {
+
+
+        // On récupère le texte du commentaire
+        $commentaire_text = post('commentaire');
+
+
+        if (empty($commentaire_text)) {
+
+            set_flash('error', 'Veuillez remplir le champ commentaire.');
+            redirect('comment/create');
+        } else {
+
+
+            // 1. On récupère l'ID de l'auteur (qui est connecté)
+            $user_id = $_SESSION['user_id'];
+
+            // 2. On appelle le Modèle pour insérer en BDD
+            if (create_comment($commentaire_text, $user_id)) {
+
+
+                set_flash('success', 'Votre commentaire a été ajouté avec succès !');
+
+
+                redirect('comment/livre_or');
+            } else {
+
+
+                set_flash('error', 'Une erreur est survenue lors de l\'ajout du commentaire.');
+                redirect('comment/create');
+            }
+        }
+    } else {
+
+
+        render('comment/create');
+    }
+}
